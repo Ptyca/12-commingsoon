7c5a81e7806d3b3639277dfa2767701bcac583dd
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-05-22 08:58:28 +0300

    copypaste

--- js/home.js
+++ js/home.js
@@ -1,4 +1,9 @@
-//import
+// IMPORT
+import { Clock } from "./components/clock/Clock.js";
+import { socials } from "./components/socials/socials.js";
+import { socialsData } from "./data/socialsData.js";
 
-
-//EXECUTION
+// EXECUTION
+new Clock('#clock_1');
+// new Clock(485545);
+socials('footer .socials', socialsData);
